18c35e48a0bb89c94537a5301f7dc05ddb4fd455
Ivlev Grigoriy <igs94@mail.ru>
2023-08-13 13:07:53 +0300

    feat: add portfolio component

---
+++ src/components/Main/Portfolio/Portfolio.jsx
@@ -0,0 +1,33 @@
+import cn from 'classnames';
+import styles from './Portfolio.module.css';
+import { Divider } from '../../Divider';
+import { Link } from '../../Link';
+
+const LINKS = [{
+  href: '', text: 'Статичный сайт',
+}, {
+  href: '', text: 'Адаптивный сайт',
+}, {
+  href: '', text: 'Одностраничное приложение',
+}];
+export function Portfolio({ className }) {
+  return (
+    <div className={cn(className, styles.portfolio)}>
+      <h2 className={styles.portfolio__title}> Портфолио</h2>
+      <ul className={styles.portfolio__list}>
+        {LINKS.map(({ href, text }, i) => (
+          <li key={href} className={styles.portfolio__item}>
+            <Link href={href} underline={false} className={styles.portfolio__link}>
+              <div className={styles['portfolio__link-text']}>
+                {text}
+              </div>
+              {i !== LINKS.length - 1
+                    && <Divider color="white" />}
+            </Link>
+          </li>
+        ))}
+
+      </ul>
+    </div>
+  );
+}
