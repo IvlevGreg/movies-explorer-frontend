ff75d6931266419417b99f1a1384e085a62116b3
Ivlev Grigoriy <igs94@mail.ru>
2023-08-15 20:23:12 +0300

    refactor: route logic

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,60 @@
-import { Main } from '../Main';
+import { Route, Routes } from 'react-router-dom';
 import { Footer } from '../Footer';
 import { Header } from '../Header';
-import { ContentRouter } from '../ContentRouter';
+import { ProtectedRouteElement } from '../ProtectedRouteElement';
+import { Main } from '../Main';
+import { Register } from '../Register';
+import { NotFoundPage } from '../NotFoundPage';
 
 export function App() {
   return (
     <div className="App">
-      <Header />
-      <ContentRouter />
-      <Footer />
+      <Routes>
+        <Route
+          path="/"
+          element={(
+            <ProtectedRouteElement loggedIn>
+              <Header />
+              <Main />
+              <Footer />
+            </ProtectedRouteElement>
+            )}
+        />
+        <Route
+          path="/movies"
+          element={(
+            <ProtectedRouteElement loggedIn>
+              <Header />
+              movies
+              <Footer />
+            </ProtectedRouteElement>
+            )}
+        />
+        <Route
+          path="/saved-movies"
+          element={(
+            <ProtectedRouteElement loggedIn>
+              <Header />
+              saved-movies
+              <Footer />
+            </ProtectedRouteElement>
+            )}
+        />
+        <Route
+          path="/profile"
+          element={(
+            <ProtectedRouteElement loggedIn>
+              <Header />
+              profile
+              <Footer />
+            </ProtectedRouteElement>
+            )}
+        />
+
+        <Route path="/signin" element={<h1>signin</h1>} />
+        <Route path="/signup" element={<Register />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
     </div>
   );
 }
