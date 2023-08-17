f32ecefe7d617b3f5c1df07d5ee3395b0899c794
Ivlev Grigoriy <igs94@mail.ru>
2023-08-17 21:37:12 +0300

    feat: add rest props for controllers

--- src/components/Controllers/NameController/NameController.jsx
+++ src/components/Controllers/NameController/NameController.jsx
@@ -2,13 +2,13 @@ import { Controller } from 'react-hook-form';
 import { REQUIRED_MESSAGE } from '../../../utils/validation';
 import { Input } from '../../Input';
 
-export function NameController({ className, control }) {
+export function NameController({ className, control, ...rest }) {
   return (
     <Controller
       name="name"
       control={control}
       rules={{ required: { value: true, message: REQUIRED_MESSAGE } }}
-      render={({ field }) => <Input {...field} className={className} />}
+      render={({ field }) => <Input {...field} {...rest} className={className} />}
     />
   );
 }
