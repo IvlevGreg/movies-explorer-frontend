2c33b61cd8010f2af321b06688886cf652d97244
Ivlev Grigoriy <igs94@mail.ru>
2023-08-19 10:16:05 +0300

    feat: add custom switcher

---
+++ src/components/Switcher/Switcher.jsx
@@ -0,0 +1,17 @@
+import cn from 'classnames';
+import { forwardRef } from 'react';
+import styles from './Switcher.module.css';
+
+export const Switcher = forwardRef(({ className, ...rest }, ref) => (
+  <label className={cn(className, styles.switcher)}>
+    <input
+      {...rest}
+      ref={ref}
+      type="checkbox"
+      className={styles.switcher__input}
+    />
+    <span
+      className={styles.switcher__switch}
+    />
+  </label>
+));
