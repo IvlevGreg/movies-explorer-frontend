79936cdcbe094757e3c4ac45f7fdca829ce36e47
Ivlev Grigoriy <igs94@mail.ru>
2023-08-26 11:44:19 +0300

    Create MainApi.js

---
+++ src/utils/Api/MainApi.js
@@ -0,0 +1,108 @@
+import { IS_DEVELOPMENT } from '../IS_DEVELOPMENT';
+
+class MainApiClass {
+  constructor({ baseUrl, headers, otherCommonProps }) {
+    this._baseUrl = baseUrl;
+    this._headers = headers;
+    this._otherCommonProps = otherCommonProps;
+  }
+
+  static _parseJson(res) {
+    return res.ok ? res.json() : Promise.reject(res);
+  }
+
+  postSignUp({ password, email }) {
+    return fetch(`${this._baseUrl}/signup`, {
+      method: 'POST',
+      headers: this._headers,
+      ...this._otherCommonProps,
+
+      body: JSON.stringify({
+        password,
+        email,
+      }),
+    }).then(MainApiClass._parseJson);
+  }
+
+  postSignOut() {
+    return fetch(`${this._baseUrl}/sign-out`, {
+      method: 'POST',
+      headers: this._headers,
+      ...this._otherCommonProps,
+
+    }).then(MainApiClass._parseJson);
+  }
+
+  postSignIn({ password, email, name }) {
+    return fetch(`${this._baseUrl}/signin`, {
+      method: 'POST',
+      headers: this._headers,
+      ...this._otherCommonProps,
+
+      body: JSON.stringify({
+        password,
+        email,
+        name,
+      }),
+    }).then((data) => MainApiClass._parseJson(data));
+  }
+
+  getUsersMe() {
+    return fetch(`${this._baseUrl}/users/me`, {
+      headers: this._headers,
+      ...this._otherCommonProps,
+
+    }).then(MainApiClass._parseJson);
+  }
+
+  updateUserData({ email, password }) {
+    return fetch(`${this._baseUrl}/users/me`, {
+      method: 'PATCH',
+      headers: this._headers,
+      ...this._otherCommonProps,
+      body: JSON.stringify({
+        password,
+        email,
+      }),
+    }).then(this._parseJson);
+  }
+
+  getMovies() {
+    return fetch(`${this._baseUrl}/movies`, {
+      headers: this._headers,
+      ...this._otherCommonProps,
+
+    }).then(MainApiClass._parseJson);
+  }
+
+  postMovie(movie) {
+    return fetch(`${this._baseUrl}/movies`, {
+      method: 'POST',
+      headers: this._headers,
+      ...this._otherCommonProps,
+
+      body: JSON.stringify({
+        ...movie,
+      }),
+    }).then(MainApiClass._parseJson);
+  }
+
+  deleteMovieById(id) {
+    return fetch(`${this._baseUrl}/movies/${id}`, {
+      method: 'DELETE',
+      headers: this._headers,
+      ...this._otherCommonProps,
+    }).then(this._parseJson);
+  }
+}
+
+export const MainApi = new MainApiClass({
+  baseUrl: IS_DEVELOPMENT ? 'http://localhost:4000' : 'https://api.blogogram.nomoreparties.sbs',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  otherCommonProps: {
+    credentials: 'include',
+    mode: 'cors',
+  },
+});
